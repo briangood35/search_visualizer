78fa1f751a078ca050e691d897233b4c521c3c55
briangood35 <brigood@umich.edu>
2022-12-23 23:21:01 -0500

    initial commit

---
+++ visualizer.py
@@ -0,0 +1,22 @@
+
+import pygame
+
+
+
+
+class Visualizer:
+    """Visualization Engine
+    """
+    def __init__(self):
+        """Initializes pygame window
+        """
+        return
+
+
+
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
