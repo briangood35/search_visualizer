334dfd679b5b478db8dd937851dd7b6f763bd225
Brian Good <brigood@umich.edu>
2022-12-27 01:06:46 -0500

    docs: added sphinx documentation
    * added sphinx documentation
    
    * updated readme
    
    * renamed search.py to visualization.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Search Visualizer
 ### This repository contains a visualization tool for breadth-first and depth-first searching algorithms.
+Full documentation located at `docs/index.html`
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+<<<<<<< HEAD
+import visualizer
+
+
+def main():
+    """Main function creates Search class instance and calls run function
+    """
+    v = visualizer.Visualizer()
+    v.run()
+=======
 import search
 
 
 def main():
     s = search.Search()
     s.run()
+>>>>>>> main
 
 if __name__ == "__main__":
     main()
